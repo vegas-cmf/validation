ef8946fcb744b06cedb373b527e5bbe62d64745c
Arek <arkadiusz.ostrycharz@gmail.com>
2014-08-12 16:46:40 +0200

    removed forms dependency, Cloneable Validator renamed and moved to vegas-cmf/forms (it's not in fact a validator but some kind of validation extender)

--- tests/Validation/Validator/SizeOfTest.php
+++ tests/Validation/Validator/SizeOfTest.php
@@ -25,75 +25,60 @@ class SizeOfTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->di = DI::getDefault();
-
         $this->form = new FakeForm();
-        $this->form->remove('fake_field');
-
-        $cloneable = $this->prepareValidCloneableField();
-        $this->form->add($cloneable);
     }
 
     public function testInvalidInput()
     {
         $model = new FakeModel();
 
-        $this->form->get('cloneable_field')->addValidator(new SizeOf(array('min' => 1, 'max' => 2)));
-        $this->form->bind(array('cloneable_field', 'notArrayedValue'), $model);
+        $this->form->get('fake_field')->addValidator(new SizeOf(array('min' => 1, 'max' => 2)));
+        $this->form->bind(array('fake_field', 'notArrayedValue'), $model);
 
         $this->assertFalse($this->form->isValid());
-        $this->assertTrue((bool)count($this->form->getMessagesFor('cloneable_field')));
+        $this->assertTrue((bool)count($this->form->getMessagesFor('fake_field')));
     }
 
     public function testValidInput()
     {
         $model = new FakeModel();
 
-        $this->form->get('cloneable_field')->addValidators(array(
+        $this->form->get('fake_field')->addValidators(array(
             new SizeOf(array('min' => 1, 'max' => 2))
         ));
 
-        $this->form->bind(array('cloneable_field' => array(
+        $this->form->bind(array('fake_field' => array(
             array('test1' => 'foo', 'test2' => 'bar'),
             array('test1' => 'foo', 'test2' => 'bar'),
             array('test1' => 'foo', 'test2' => 'bar')
         )), $model);
 
         $this->assertFalse($this->form->isValid());
-        $this->assertTrue((bool)count($this->form->getMessagesFor('cloneable_field')));
+        $this->assertTrue((bool)count($this->form->getMessagesFor('fake_field')));
 
-        $this->form->bind(array('cloneable_field' => array(
+        $this->form->bind(array('fake_field' => array(
             array('test1' => 'foo', 'test2' => 'bar'),
             array('test1' => 'foo', 'test2' => 'bar'),
         )), $model);
 
         $this->assertTrue($this->form->isValid());
 
-        $this->form->bind(array('cloneable_field' => array(
+        $this->form->bind(array('fake_field' => array(
             array('test1' => 'foo', 'test2' => 'bar')
         )), $model);
 
         $this->assertTrue($this->form->isValid());
 
-        $this->form->bind(array('cloneable_field' => array(
+        $this->form->bind(array('fake_field' => array(
             array('test1' => 'foo', 'test2' => '')
         )), $model);
 
         $this->assertTrue($this->form->isValid());
 
-        $this->form->bind(array('cloneable_field' => array(
+        $this->form->bind(array('fake_field' => array(
             array('test1' => '', 'test2' => '')
         )), $model);
 
         $this->assertFalse($this->form->isValid());
-    }
-
-    private function prepareValidCloneableField()
-    {
-        $cloneable = new Cloneable('cloneable_field');
-        $cloneable->setAssetsManager($this->di->get('assets'));
-        $cloneable->setBaseElements(array(new \Phalcon\Forms\Element\Text('test1')));
-        $cloneable->addBaseElement(new \Phalcon\Forms\Element\Text('test2'));
-
-        return $cloneable;
     }
 }
