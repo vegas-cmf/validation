eeebf38ba758f8632f4ecb867b175979d8474fd3
Mateusz Aniolek <matty201@gmail.com>
2014-10-08 13:07:19 +0200

    doc fixes

--- src/Validator/Date/Exception.php
+++ src/Validator/Date/Exception.php
@@ -2,9 +2,9 @@
 /**
  * This file is part of Vegas package
  *
- * @author Mateusz Aniołek <dev@mateusz-aniolek.com>
+ * @author Mateusz Aniołek <mateusz.aniolek@amsterdam-standard.pl>
  * @copyright Amsterdam Standard Sp. Z o.o.
- * @homepage https://bitbucket.org/amsdard/vegas-phalcon
+ * @homepage http://vegas-cmf.github.io/
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
